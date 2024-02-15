dc8d32e6ba841225b435057d3b0023291faf45f1
Durga Prasad Baghuthu <dbaghuthu@betalen.in>
2024-02-15 19:17:44 +0530

    db setup and context setup

---
+++ Clarity.Web.Service/DBConfiguration/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Clarity.Web.Service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clarity.Web.Service.DBConfiguration
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Roles> roles { get; set; }
+    }
+}
