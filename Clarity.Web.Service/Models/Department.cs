186ad0b74b368eb82ffa308eb7288d85c296ada8
Surbhi Jambhulkar <Surbhi.Jambhulkar@betalen.in>
2024-02-15 19:31:44 +0530

    Department Table Added

---
+++ Clarity.Web.Service/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Clarity.Web.Service.Models
+{
+    [Table("Department")]
+    public class Department: Common
+    {
+        [Key]
+        public long DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DepartmentCode { get; set; }
+    }
+}
