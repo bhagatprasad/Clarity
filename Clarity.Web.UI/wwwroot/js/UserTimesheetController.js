375e2dd2875a2f2fd00f9555d5ccc9861a444703
Durga Prasad Baghuthu <dbaghuthu@betalen.in>
2024-04-21 15:34:14 +0530

    commit

---
+++ Clarity.Web.UI/wwwroot/js/UserTimesheetController.js
@@ -0,0 +1,33 @@
+﻿function UserTimesheetController() {
+    var self = this;
+    self.ApplicationUser = {};
+    var requests = [];
+    var actions = [];
+    self.Employees = [];
+    actions.push(serviceUrls.getUsers);
+    actions.push(serviceUrls.fetchAllTaskItems);
+    self.init = function () {
+        var appuser = storageService.get("ApplicationUser");
+        if (appuser) {
+            self.ApplicationUser = appuser;
+        }
+        for (var i = 0; i < actions.length; i++) {
+            var ajaxConfig = {
+                url: actions[i],
+                method: 'GET',
+            };
+            requests.push($.ajax(ajaxConfig));
+        }
+        $.when.apply($, requests).done(function () {
+            var responses = arguments;
+            if (responses[0][0] && responses[0][0].data) {
+                responses[0][0].data.forEach(function (item) {
+                    self.Employees.push(item.employee);
+                });
+            }
+            console.log(responses);
+        }).fail(function () {
+            console.log('One or more requests failed.');
+        });
+    };
+}
